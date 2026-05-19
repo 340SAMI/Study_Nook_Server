801971dc349f2011bf4d1c82456e8e463bb4befc
340SAMI <aminulislamsami1@gmail.com>
2026-05-19 12:20:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.port|| 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+}); 
+
+
+app.listen(PORT, ()=>{
+    console.log("server is running");
+})
